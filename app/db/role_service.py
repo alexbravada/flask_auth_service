19a9fc64c3406232bcfa839879de28b7fe379b47
boris <boris.vladimirov13@gmail.com>
2022-11-09 17:22:47 +0600

    REST API done

--- app/db/role_service.py
+++ app/db/role_service.py
@@ -1,10 +1,13 @@
 from datetime import datetime
+from copy import deepcopy
 
 from db.pg_base import PostgresService
 from models.user import User, Role, UserRole
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import NoResultFound, MultipleResultsFound
 from werkzeug.exceptions import HTTPException, BadRequest
+from flask import abort
+from flask import make_response, jsonify
 
 
 class RoleService(PostgresService):
@@ -14,57 +17,64 @@ class RoleService(PostgresService):
     def add_role(self, name, description):
         with Session(self.engine) as session:
             if not name:
-                raise BadRequest(description=f'''field 'name' must be specified''')
+                abort(400)
             try:
                 role = session.query(Role).filter(Role.name == name).one()
                 if role:
-                    raise BadRequest(description=f'''Role with name={name} exist''')
+                    abort(400)
             except NoResultFound:
                 added_role = Role()
                 added_role.name = name
                 added_role.description = description
-
                 session.add(added_role)
                 session.commit()
-
-                return f'''Role name={name}, description={description if description else '<not specified>'} created'''
+                return session.query(Role).filter(Role.name == name).one()
 
     def del_role(self, role_id):
         with Session(self.engine) as session:
             try:
-                session.query(Role).filter(Role.id == role_id).delete()
+                role = session.query(Role).filter(Role.id == role_id).one()
+                session.query(Role).filter(Role.id == role.id).delete()
                 session.commit()
-                return f'''Role with id={role_id} deleted'''
+                return role
             except NoResultFound:
-                raise BadRequest(description=f'''Role with id={role_id} doesn't exist''')
+                abort(404)
 
     def update_role(self, role_id, name, description):
         with Session(self.engine) as session:
-            try:
+            if not name and not description:  # check if name and description presented in request
+                abort(400)
+            try:  # check if ID exist
                 session.query(Role).filter(Role.id == role_id).one()
             except NoResultFound:
-                raise BadRequest(description=f'''Role with id={role_id} doesn't exist''')
+                abort(404)
+            try:  # check if name exist - if exist aborting
+                role = session.query(Role).filter(Role.name == name).one()
+                if role:
+                    abort(400)
+            except MultipleResultsFound:
+                abort(400)
+            except NoResultFound:
+                if not name:  # only description presented
+                    session.query(Role).filter(Role.id == role_id).update(
+                        {'description': description, 'modified': datetime.utcnow()}
+                    )
+                    session.commit()
+                    role = session.query(Role).filter(Role.id == role_id).one()
+                elif not description:  # only name presented
+                    session.query(Role).filter(Role.id == role_id).update(
+                        {'name': name, 'modified': datetime.utcnow()}
+                    )
+                    session.commit()
+                    role = session.query(Role).filter(Role.id == role_id).one()
+                else:  # name and description presented
+                    session.query(Role).filter(Role.id == role_id).update(
+                        {'name': name, 'description': description, 'modified': datetime.utcnow()}
+                    )
+                    session.commit()
+                    role = session.query(Role).filter(Role.id == role_id).one()
 
-            if not name and not description:
-                raise BadRequest(description=f'''Nothing to update''')
-            elif not name:
-                session.query(Role).filter(Role.id == role_id).update(
-                    {'description': description, 'modified': datetime.utcnow()}
-                )
-                session.commit()
-            elif not description:
-                session.query(Role).filter(Role.id == role_id).update(
-                    {'name': name, 'modified': datetime.utcnow()}
-                )
-                session.commit()
-            else:
-                session.query(Role).filter(Role.id == role_id).update(
-                    {'name': name, 'description': description, 'modified': datetime.utcnow()}
-                )
-                session.commit()
-
-            return f'Role updated, for id={role_id} new values name={name if name else "<not specified>"}, ' \
-                   f'description={description if description else "<not specified>"}'
+                return role
 
     def show_all_roles(self):
         with Session(self.engine) as session:
@@ -73,73 +83,71 @@ class RoleService(PostgresService):
     def show_role(self, role_id):
         with Session(self.engine) as session:
             try:
-                return session.query(Role).filter(Role.id == role_id).all()
+                return session.query(Role).filter(Role.id == role_id).one()
             except NoResultFound:
-                raise BadRequest(description=f'''Role with id={role_id} doesn't exist''')
+                abort(404)
 
     def user_add_role(self, user_id, role_id):
         with Session(self.engine) as session:
-            try:
-                user = session.query(User).filter(User.id == user_id).one()
+            try:  # check User model for user_id exist, if not - aborting
+                session.query(User).filter(User.id == user_id).one()
             except NoResultFound:
-                raise BadRequest(description=f'''User with id={user_id} doesn't exist''')
+                abort(404)
 
-            try:
-                role = session.query(Role).filter(Role.id == role_id).one()
+            try:  # check Role model for role_id exist, if not - aborting
+                session.query(Role).filter(Role.id == role_id).one()
             except NoResultFound:
-                raise BadRequest(description=f'''Role with id={role_id} doesn't exist''')
-
-            try:
-                user_role = session.query(UserRole).filter(UserRole.user_id == user_id,
-                                                           UserRole.role_id == role_id).one()
-                raise BadRequest(description=f'''UserRole with User.id={user_id}, Role.id={role_id} exist''')
-            except MultipleResultsFound:
-                raise BadRequest(description=f'''UserRole with User.id={user_id}, Role.id={role_id} multiple exist''')
+                abort(404)
+            try:  # check if relationship user_id__role_id exist in UserRole model, if not - add new
+                session.query(UserRole).filter(UserRole.user_id == user_id,
+                                               UserRole.role_id == role_id).one()
+                abort(400)
+            except MultipleResultsFound:  # this exceptions occurs if UserRole model has multiple identical entries
+                abort(400)
             except NoResultFound:
                 user_add_role = UserRole()
                 user_add_role.user_id = user_id
                 user_add_role.role_id = role_id
                 session.add(user_add_role)
                 session.commit()
-                return f'user_role row with user_id={user_id}, role_id={role_id} created'
+                return session.query(UserRole).filter(UserRole.user_id == user_id,
+                                                      UserRole.role_id == role_id).one()
 
     def user_remove_role(self, user_id, role_id):
         with Session(self.engine) as session:
-            if not user_id or not role_id:
-                raise BadRequest(description=f''''user_id' and 'role_id' must be specified''')
-
+            # можно и нужно доработать каскадное удаление (через модели) и
+            # этот метод можно будет использовать только для очистки старых записей
+            if not user_id or not role_id:  # check if user_id or role_id presented in request
+                abort(400)
             try:
                 user_role = session.query(UserRole). \
                     filter(UserRole.user_id == user_id,
                            UserRole.role_id == role_id).one()
                 session.query(UserRole).filter(UserRole.id == user_role.id).delete()
                 session.commit()
-                return f'UserRole with id={user_role.id} deleted'
-            except NoResultFound:
-                raise BadRequest(
-                    description=f'''UserRole with 'user_id'={user_id}, 'role_id'={role_id} doesn't exist''')
-
-    def user_check_role(self, user_id, role_id):
-        with Session(self.engine) as session:
-            # try:
-            #     user = session.query(User).filter(User.id == user_id).one()
-            # except NoResultFound:
-            #     raise BadRequest(description=f'''User with id={user_id} doesn't exist''')
-            #
-            # try:
-            #     role = session.query(Role).filter(Role.id == role_id).one()
-            # except NoResultFound:
-            #     raise BadRequest(description=f'''Role with id={role_id} doesn't exist''')
-            # TODO: это надо убрать, но нужно сделать каскадное удаление в таблице user__role при удалении user или role соответствующих
-
-            try:
-                user_role = session.query(UserRole).filter(
-                    UserRole.user_id == user_id,
-                    UserRole.role_id == role_id).one()
                 return user_role
             except NoResultFound:
-                raise BadRequest(
-                    description=f'''user_role row with user_id={user_id}, role_id={role_id} doesn't exist''')
+                abort(404)
+
+    def user_check_role(self, user_id):
+        with Session(self.engine) as session:
+            try:
+                user_role = session.query(UserRole).filter(UserRole.user_id == user_id).all()
+                if len(user_role) == 0:
+                    abort(404)
+                return user_role
+            except NoResultFound:
+                abort(404)
+
+    def role_check_user(self, role_id):
+        with Session(self.engine) as session:
+            try:
+                role_user = session.query(UserRole).filter(UserRole.role_id == role_id).all()
+                if len(role_user) == 0:
+                    abort(404)
+                return role_user
+            except NoResultFound:
+                abort(404)
 
     def user_role_show_all(self):
         with Session(self.engine) as session:

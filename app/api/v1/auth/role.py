6de0188bd2e038adebb0dfc44f0f6c8e15f0b67c
boris <boris.vladimirov13@gmail.com>
2022-11-09 17:22:47 +0600

    REST API done

--- app/api/v1/auth/role.py
+++ app/api/v1/auth/role.py
@@ -4,7 +4,8 @@ from pprint import pprint
 from flask import Blueprint
 from flask import request
 from flask import jsonify
-
+from flask import make_response
+from flask import abort
 
 from flask_jwt_extended import JWTManager
 from flask_jwt_extended import verify_jwt_in_request
@@ -13,54 +14,66 @@ from flask_jwt_extended import get_jwt_identity
 
 from db.role_service import RoleService
 
-
 role_bp = Blueprint('role', __name__, url_prefix='/role')
 
 
-#TODO add JWT required to each route
+# TODO add JWT required to each route
+@role_bp.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@role_bp.errorhandler(400)
+def not_found(error):
+    return make_response(jsonify({'error': 'Request data is invalid'}), 400)
 
 
 @role_bp.route("/add", methods=['POST'])
 def add_role():
     '''curl -X POST -H "Content-Type: application/json" -d '{"name":"role_name33", "description":"descrip"}' http://127.0.0.1:5000/api/v1/auth/role/add'''
+    if not request.json or not 'name' in request.json:
+        abort(400)
     result = request.json
     name = result.get('name')
     description = result.get('description')
     db = RoleService()
-    response = db.add_role(name, description)
-    print(response)
-    return response, 201
+    response_array = [db.add_role(name, description).as_dict]
+    return jsonify({'role': response_array}), 201
 
 
-@role_bp.route('/delete', methods=['POST'])
+@role_bp.route('/delete', methods=['DELETE'])
 def delete_role():
+    if not request.json or not 'id' in request.json:
+        abort(400)
     result = request.json
     role_id = result.get('id')
     db = RoleService()
-    return db.del_role(role_id)
+    response_array = [db.del_role(role_id).as_dict]
+    return jsonify({'deleted role': response_array})
 
 
-@role_bp.route('/show_all', methods=['POST', 'GET'])
+@role_bp.route('', methods=['GET'])
 def show_roles_all():
     db = RoleService()
     response_inner = [x.as_dict for x in db.show_all_roles()]
-    return jsonify(response_inner)
+    return jsonify({'roles': response_inner})
 
 
-@role_bp.route('/show', methods=['POST', 'GET'])
-def show_role():
-    result = request.json
-    role_id = result.get('id')
+@role_bp.route('/<int:role_id>', methods=['GET'])
+def show_role(role_id):
     db = RoleService()
-    response_inner = [x.as_dict for x in db.show_role(role_id)]
-    return jsonify(response_inner)
+    response_inner = [db.show_role(role_id).as_dict]
+    return jsonify({'role': response_inner})
 
 
-@role_bp.route('/update', methods=['POST'])
+@role_bp.route('/update', methods=['PUT'])
 def update_role():
+    if not request.json or not 'id' in request.json:
+        abort(400)
     result = request.json
     role_id = result.get('id')
     name = result.get('name')
     description = result.get('description')
     db = RoleService()
-    return db.update_role(role_id, name, description)
+    response_inner = [db.update_role(role_id, name, description).as_dict]
+    return jsonify({'role updated': response_inner}), 201
--- app/api/v1/auth/user_role.py
+++ app/api/v1/auth/user_role.py
@@ -4,6 +4,8 @@ from pprint import pprint
 from flask import Blueprint
 from flask import request
 from flask import jsonify
+from flask import make_response
+from flask import abort
 
 from flask_jwt_extended import create_access_token, create_refresh_token
 from flask_jwt_extended import JWTManager
@@ -13,42 +15,64 @@ from flask_jwt_extended import get_jwt_identity
 
 from db.role_service import RoleService
 
-
 user_role_bp = Blueprint('user_role', __name__, url_prefix='/user/role')
 
 
-#TODO add JWT required to each route
+# TODO add JWT required to each route
+
+@user_role_bp.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
-@user_role_bp.route('/user_role_show_all', methods=['POST', 'GET'])
-def show_role():
+@user_role_bp.errorhandler(400)
+def not_found(error):
+    return make_response(jsonify({'error': 'Request data is invalid'}), 400)
+
+
+def _json_check(rrequest):
+    if not rrequest.json or not 'user_id' in rrequest.json or not 'role_id' in rrequest.json:
+        abort(400)
+
+
+@user_role_bp.route('', methods=['GET'])
+def show_user_role():
     db = RoleService()
     response_inner = [x.as_dict for x in db.user_role_show_all()]
-    return jsonify(response_inner)
+    return jsonify({'user__roles': response_inner})
+
 
 @user_role_bp.route('/user_role_add', methods=['POST'])
 def user_add_role():
+    _json_check(request)
     result = request.json
     user_id = result.get('user_id')
     role_id = result.get('role_id')
     db = RoleService()
-    return db.user_add_role(user_id, role_id)
+    response_inner = [db.user_add_role(user_id, role_id).as_dict]
+    return jsonify({'user__role created': response_inner})
 
 
-@user_role_bp.route('/user_role_show', methods=['POST'])
-def user_check_role():
-    result = request.json
-    user_id = result.get('user_id')
-    role_id = result.get('role_id')
+@user_role_bp.route('/user_role_show/<int:user_id>', methods=['GET'])
+def user_check_role(user_id):
     db = RoleService()
-    response_inner = [x.as_dict for x in db.user_check_role(user_id, role_id)]
-    return jsonify(response_inner)
+    response_inner = [x.as_dict for x in db.user_check_role(user_id)]
+    return jsonify({'user__role': response_inner})
 
 
-@user_role_bp.route('/user_role_delete', methods=['POST'])
+@user_role_bp.route('/role_user_show/<int:role_id>', methods=['GET'])
+def role_check_user(role_id):
+    db = RoleService()
+    response_inner = [x.as_dict for x in db.role_check_user(role_id)]
+    return jsonify({'role__user': response_inner})
+
+
+@user_role_bp.route('/user_role_delete', methods=['DELETE'])
 def user_role_remove():
+    _json_check(request)
     result = request.json
     user_id = result.get('user_id')
     role_id = result.get('role_id')
     db = RoleService()
-    return db.user_remove_role(user_id, role_id)
+    response_inner = [db.user_remove_role(user_id, role_id).as_dict]
+    return jsonify({'user__role deleted': response_inner})
